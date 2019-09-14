0a36ead140a4671ae39573d12c0fa1cb4f9d0d2e
Vinicio Liuti <viniliuti@live.com>
2019-09-14 18:14:22 -0300

    add project webApi with Entity to Postgres

---
+++ todoApi/Models/TodoContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace todoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
